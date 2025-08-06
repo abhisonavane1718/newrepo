81e430949777641f4b290e62302e090768da21d4
Abhishek Sonavane <abhisheksonavane1718@gmail.com>
2025-08-06 14:49:05 +0530

    done

---
+++ git.java
@@ -0,0 +1,7 @@
+package newrepo;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println(" its a cow who is a dog who eats cats on tree like horse sitting on garage while repairing buffaloe legs.");
+    }
+}
